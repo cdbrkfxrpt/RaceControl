d81b099cff93c278d9cfaf580c60a9309e4b61c6
Florian Eich <flrn@nrmncr.net>
2015-12-26 20:42:38 +0100

    Growing.

--- connectedrace/connectedrace.py
+++ connectedrace/connectedrace.py
@@ -3,6 +3,22 @@ import logger
 import antenna
 
 def main():
+
+    # pseudocode for main:
+    #
+    # can_interfaces = ['vcan0'] # TODO: scan system for interfaces, don't hardcode
+    # serve_port = 5252
+    # logfiles = ['log.csv', 'log.db'] # TODO: loggings have to be created per run, CSV doesn't yet
+    #
+    # cableslave = cable.Handler(can_interfaces)
+    # antennaslave = antenna.Handler(serve_port)
+    # loggingslave = logger.Handler(logfiles)
+    #
+    # cableslave.shoot(list_of(antennaslave.targets, loggingslave.targets))
+    # antennaslave.shoot(list_of(cableslave.targets, loggingslave.targets))
+    #
+    # massacre(cableslave, antennaslave, loggingslave)
+    #
     can_interface = 'vcan0'
     bus = can.interface.Bus(can_interface, bustype='socketcan_native')
     # msg = can.Message(arbitration_id=0xc0ffee,
