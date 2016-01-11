e501b39b156d0ab66977267e57001d7092075dee
Florian Eich <flrn@nrmncr.net>
2016-01-11 17:12:51 +0100

    And another one.

--- connectedrace/bucket.py
+++ connectedrace/bucket.py
@@ -6,7 +6,7 @@ class BucketHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         print('life sign from BucketHandler')
         print('self.client_address ', self.client_address[0])
-        if self.client_address in self.server.antennad.nodes:
+        if self.client_address[0] in self.server.antennad.nodes:
             msglist = self.rfile.read().split(b'#')
             msglist = list(msg for msg in msglist
                                if msg and not msg in PROTOCOL)
@@ -19,13 +19,13 @@ class BucketHandler(socketserver.DatagramRequestHandler):
                 print('Received message ', msg)
         elif (self.rfile.readline().strip() == PROTOCOL[0]
             and not self.client_address[0] == self.server.antennad.ip):
-            self.server.antennad.add_node(self.client_address)
+            self.server.antennad.add_node(self.client_address[0])
             self.wfile.write(PROTOCOL[1])
-            print('UDP/Node acknowledged: ', self.client_address)
+            print('UDP/Node acknowledged: ', self.client_address[0])
         elif (self.rfile.readline().strip() == PROTOCOL[1]
             and not self.client_address[0] == self.server.antennad.ip):
-            self.server.antennad.add_node(self.client_address)
-            print('UDP/Node registered: ', self.client_address)
+            self.server.antennad.add_node(self.client_address[0])
+            print('UDP/Node registered: ', self.client_address[0])
 
 class Bucket(socketserver.UDPServer):
     def __init__(self, server_address, BucketHandlerClass, antennad):
