5c5995fa5b83081f3e2e49fc1333297df914c2bc
Florian Eich <flrn@nrmncr.net>
2016-01-11 17:59:21 +0100

    hey

--- connectedrace/bucket.py
+++ connectedrace/bucket.py
@@ -5,26 +5,26 @@ from globals import D_PORT, PROTOCOL
 class BucketHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         print('self.client_address ', self.client_address)
-        print('self.rfile.read().strip()', self.rfile.read().strip())
+        node_msg = self.rfile.read().strip()
+        print('self.rfile.read().strip()', node_msg)
         if self.client_address[0] in self.server.antennad.nodes:
-            msglist = self.rfile.read().split(b'#')
-            msglist = list(msg for msg in msglist
+            node_msg = list(msg for msg in node_msg
                                if msg and not msg in PROTOCOL)
-            for msg in msglist:
+            for msg in node_msg:
                 try:
                     self.server.antennad.notify(pickle.loads(msg))
                 except pickle.UnpicklingError:
                     # syslog.write('corrupted data')
                     print('corrupted data received')
                 print('Received message ', msg)
-        elif (self.rfile.readline().strip() == PROTOCOL[0]
+        elif (node_msg == PROTOCOL[0]
             and not self.client_address[0] == self.server.antennad.ip):
             self.server.antennad.add_node(self.client_address[0])
             print('UDP/Node acknowledged: ', self.client_address[0])
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.sendto(PROTOCOL[1], (self.client_address[0], D_PORT))
             print('Answered ', PROTOCOL[1])
-        elif (self.rfile.readline().strip() == PROTOCOL[1]
+        elif (node_msg == PROTOCOL[1]
             and not self.client_address[0] == self.server.antennad.ip):
             self.server.antennad.add_node(self.client_address[0])
             print('UDP/Node registered: ', self.client_address[0])
