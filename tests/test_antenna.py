4c7cfac55f645649dfb1d37a1abcddcbf0788034
Florian Eich <florian.eich@bmw-motorsport.com>
2016-02-23 17:08:42 +0100

    Corrected tests. Still not working though.

--- tests/test_antenna.py
+++ tests/test_antenna.py
@@ -2,12 +2,10 @@ import connectedrace
 import can
 
 testmessage = can.Message(data=[1,2,3,4,5,6,7,8])
+node = connectedrace.Node('127.0.0.1', testmessage)
 
 class NodeTest:
-    def __init__(self):
-        node = Node('127.0.0.1', testmessage)
-
     def test_one(self):
-        assert self.last_msg.isinstance(can.Message)
+        assert isinstance(self.last_msg, can.Message)
         assert self.ip == '127.0.0.1'
-        assert self.timestamp
+        assert isinstance(self.timestamp, float)
